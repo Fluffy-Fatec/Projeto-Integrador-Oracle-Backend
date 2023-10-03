173eca98fc8cafded8643146aca4f64a025d4f5e
luizborges17 <lfborgesrp1@gmail.com>
2023-10-02 21:26:03 -0300

    kan-87 tables and sequences of module suppliers

--- backend/src/main/java/com/fluffy/backend/BackendApplication.java
+++ backend/src/main/java/com/fluffy/backend/BackendApplication.java
@@ -9,6 +9,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 
 public class BackendApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
