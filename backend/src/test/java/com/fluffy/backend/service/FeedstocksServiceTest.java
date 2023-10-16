c9f0821c0bb8ab71ff16e54c5f7a38700b094b47
Tiago Camillo <66873418+tiagocamillo@users.noreply.github.com>
2023-10-15 21:32:44 -0300

    Feedstock adjust

--- backend/src/test/java/com/fluffy/backend/service/FeedstocksServiceTest.java
+++ backend/src/test/java/com/fluffy/backend/service/FeedstocksServiceTest.java
@@ -10,10 +10,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import com.fluffy.backend.DTO.FeedstocksListDTO;
 import com.fluffy.backend.entity.Feedstocks;
+import com.fluffy.backend.entity.SupplierFeedstock;
 import com.fluffy.backend.repository.FeedstocksRepository;
+import com.fluffy.backend.repository.SupplierFeedstockRepository;
 
 public class FeedstocksServiceTest {
 	@InjectMocks
@@ -22,6 +26,9 @@ public class FeedstocksServiceTest {
 	@Mock
 	private FeedstocksRepository feedstocksRepository;
 
+	@Mock
+	private SupplierFeedstockRepository supplierFeedstockRepository;
+	
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.initMocks(this);
@@ -29,6 +36,7 @@ public class FeedstocksServiceTest {
 
 	@Test
 	public void testGetAllFeedstocks() {
+		
 		List<Feedstocks> feedstocksList = new ArrayList<>();
 
 		Feedstocks feedstock1 = new Feedstocks();
@@ -44,14 +52,19 @@ public class FeedstocksServiceTest {
 		feedstocksList.add(feedstock1);
 		feedstocksList.add(feedstock2);
 
+		SupplierFeedstock supplierfeedstock = new SupplierFeedstock();
+		supplierfeedstock.setPrice(Double.valueOf("50"));
+		
 		when(feedstocksRepository.findAll()).thenReturn(feedstocksList);
+		when(supplierFeedstockRepository.findByFeedstocks(Mockito.any())).thenReturn(supplierfeedstock);
+		
+		
+		List<FeedstocksListDTO> feedstocksListDTO = feedstocksService.getAllFeedstocks();
 
-		List<Feedstocks> result = feedstocksService.getAllFeedstocks();
-
-		assertEquals(2, result.size()); // Verifique o tamanho da lista
-		assertEquals("Feedstock1", result.get(0).getName()); // Verifique o nome do primeiro feedstock
-		assertEquals(100.0, result.get(0).getAmountAvailable()); // Verifique a quantidade disponível do primeiro
+		assertEquals(2, feedstocksListDTO.size()); // Verifique o tamanho da lista
+		assertEquals("Feedstock1", feedstocksListDTO.get(0).getName()); // Verifique o nome do primeiro feedstock
+		assertEquals(100.0, feedstocksListDTO.get(0).getAmountAvailable()); // Verifique a quantidade disponível do primeiro
 																	// feedstock
-		assertEquals("Kg", result.get(0).getMeasurement()); // Verifique a unidade de medida do primeiro feedstock
+		assertEquals("Kg", feedstocksListDTO.get(0).getMeasurement()); // Verifique a unidade de medida do primeiro feedstock
 	}
 }
